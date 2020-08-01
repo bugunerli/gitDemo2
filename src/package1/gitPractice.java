afc3de25e61755451acc9ba1dfbcba6f00390a58
bulent10 <gunerlibulent@gmail.com>
2020-08-01 16:53:35 -0600

    Uptade1.2

--- src/package1/gitPractice.java
+++ src/package1/gitPractice.java
@@ -3,8 +3,7 @@ package package1;
 public class gitPractice {
     public static void main(String[] args) {
 
-        System.out.println("Practice");
-        System.out.println("Practice");
+
         System.out.println("Practice");
         System.out.println("Practice");
 
