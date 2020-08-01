08db31a3627038377c57a0d4a34e4d8c5ab17b8f
bugunerli <69088589+bugunerli@users.noreply.github.com>
2020-08-01 16:40:59 -0600

    Update PracticeWithHasan.java

--- src/package3/PracticeWithHasan.java
+++ src/package3/PracticeWithHasan.java
@@ -3,6 +3,7 @@ package package3;
 public class PracticeWithHasan {
     public static void main(String[] args) {
         System.out.println("We live in Canada");
+        System.out.println("Practice for first of August");
     }
 
 }
