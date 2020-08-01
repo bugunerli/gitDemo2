0a8fd4bb3c893fd0ba2632c0591bebd78a652688
bulent10 <gunerlibulent@gmail.com>
2020-08-01 16:26:53 -0600

    Exercise1_1

--- src/package2/Exercise1_1.java
+++ src/package2/Exercise1_1.java
@@ -4,7 +4,7 @@ public class Exercise1_1 {
 
     public static void main(String[] args) {
         System.out.println("Practicing git");
-        System.out.println("Eid mubarek");
+        System.out.println("Eid mubarek...");
 
 
     }
